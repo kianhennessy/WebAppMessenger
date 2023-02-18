6af74bd41309038187098420437c21f621198812
Kian Hennessy <66495302+kianhennessy@users.noreply.github.com>
2023-02-18 18:39:06 +0000

    CSS for login page
    CSS for login page added

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,14 @@
 import React from "react";
+// login method icons
 import {GoogleOutlined} from "@ant-design/icons";
 
 const Login = () => {
     return (
-        <div >
-            <div >
+        <div id="login-page">
+            <div id="login-box">
                 <h3>Chat</h3>
 
-
-                <div >
+                <div className="button-login google-button">
                     <GoogleOutlined /> Sign in with Google
                 </div>
                 <br />
