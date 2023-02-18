c8e29e75b1f7392660da3aebb2854516c7607db2
Kian Hennessy <66495302+kianhennessy@users.noreply.github.com>
2023-02-18 18:20:47 +0000

    base login page
    basic login page added. No functionality currently. Issue with old react DOM. Switch is no longer used.

---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {GoogleOutlined} from "@ant-design/icons";
+
+const Login = () => {
+    return (
+        <div >
+            <div >
+                <h3>Chat</h3>
+
+
+                <div >
+                    <GoogleOutlined /> Sign in with Google
+                </div>
+                <br />
+
+            </div>
+        </div>
+    );
+}
+
+export default Login;
